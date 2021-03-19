7cd836923c8ea583e7d9cd2821c981b89ff3022a
cz.lee <cz.lee@chelpis.com>
2021-03-19 13:26:38 +0800

    fix no match route

--- src/router.tsx
+++ src/router.tsx
@@ -1,40 +1,32 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { useService } from "src/services";
-import { DefaultLayout } from "src/layouts";
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { useService } from 'src/services'
+import { DefaultLayout } from 'src/layouts'
 
 const NoMatch = () => {
-  return <div>Not found.</div>;
-};
-
-const Routes = () => {
-  const { features } = useService();
-  return (
-    <>
-      {features
-        .flatMap((feature) => feature.pages)
-        .map((page) => (
-          <Route key={page.route} exact path={page.route}>
-            {page.layout ? (
-              <page.layout>{page.component}</page.layout>
-            ) : (
-              <DefaultLayout>{page.component}</DefaultLayout>
-            )}
-          </Route>
-        ))}
-      <Route path="*">
-        <NoMatch />
-      </Route>
-    </>
-  );
-};
+  return <div>Not found.</div>
+}
 
 export const RouterProvider = () => {
+  const { features } = useService()
   return (
     <Router>
       <Switch>
-        <Routes />
+        {features
+          .flatMap((feature) => feature.pages)
+          .map((page) => (
+            <Route key={page.route} exact path={page.route}>
+              {page.layout ? (
+                <page.layout>{page.component}</page.layout>
+              ) : (
+                <DefaultLayout>{page.component}</DefaultLayout>
+              )}
+            </Route>
+          ))}
+        <Route path='*'>
+          <NoMatch />
+        </Route>
       </Switch>
     </Router>
-  );
-};
+  )
+}
