2e47dffbe20a2ca2901aa092acc77d28b6aca143
cz.lee <cz.lee@chelpis.com>
2021-03-25 16:51:35 +0800

    add favicons

---
+++ src/scripts/favicon.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+const path = require('path')
+
+const servicePath = path.resolve('./dist', process.env.SERVICE)
+
+const favicons = fs
+  .readdirSync(servicePath)
+  .filter(
+    (file) =>
+      (file.startsWith('apple') || file.startsWith('android') || file.startsWith('favicon')) &&
+      (file.split('.')[1] === 'png' || file.split('.')[1] === 'ico')
+  )
+
+favicons.forEach((file) => {
+  fs.renameSync(
+    path.resolve(servicePath, file),
+    path.resolve(servicePath, 'assets', file)
+  )
+})
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,64 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import replace from '@rollup/plugin-replace'
+import favicon from 'rollup-plugin-favicons'
+import html from 'vite-plugin-html'
+
+const getFaviconLinks = () => {
+  const favicons = [
+    { rel: 'icon', name: 'favicon', sizes: [16, 32, 48], extension: 'png' },
+    {
+      rel: 'apple-touch-icon',
+      name: 'apple-touch-icon',
+      sizes: [57, 60, 72, 76, 114, 120, 144, 152, 167, 180, 1024],
+      extension: 'png',
+    },
+    {
+      rel: 'android-chrome',
+      name: 'android-chrome',
+      sizes: [36, 48, 72, 96, 144, 192, 256, 384, 512],
+      extension: 'png',
+    },
+  ]
+
+  let faviconLinks: {
+    tag: string
+    attrs: { rel: string; type: string; href: string; size?: string }
+  }[] = [
+    {
+      tag: 'link',
+      attrs: { rel: 'icon', type: 'image/svg+xml', href: '/assets/favicon.ico' },
+    },
+    {
+      tag: 'link',
+      attrs: { rel: 'apple-touch-icon', type: 'image/png', href: '/assets/apple-touch-icon.png' },
+    },
+    {
+      tag: 'link',
+      attrs: {
+        rel: 'apple-touch-icon',
+        type: 'image/png',
+        href: '/assets/apple-touch-icon-precomposed.png',
+      },
+    },
+  ]
+
+  favicons.forEach((favicon) =>
+    favicon.sizes.forEach((size) =>
+      faviconLinks.push({
+        tag: 'link',
+        attrs: {
+          rel: favicon.rel,
+          type: `image/${favicon.extension}`,
+          size: `${size}x${size}`,
+          href: `/assets/${favicon.name}-${size}x${size}.${favicon.extension}`,
+        },
+      })
+    )
+  )
+
+  return faviconLinks
+}
 
 const mode = process.env.NODE_ENV
 const service = process.env.SERVICE
@@ -32,5 +90,26 @@ export default defineConfig({
           }),
         ]
       : []),
+    favicon({
+      source: `src/services/${service}/assets/favicon.png`,
+      configuration: {
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: false,
+          coast: false,
+          favicons: true,
+          firefox: false,
+          windows: false,
+          yandex: false,
+        },
+      },
+    }),
+    html({
+      inject: {
+        injectData: { title: 'Hello World' },
+        tags: [...getFaviconLinks()],
+      },
+    }),
   ],
 })
